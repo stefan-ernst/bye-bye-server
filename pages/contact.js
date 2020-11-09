89a5ef70078e91d750cb008332ba362be9dccd37
Stefan Ernst <ernst.stefan@icloud.com>
2020-11-09 16:55:36 +0100

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -87,6 +87,7 @@ export default function Home() {
                     CH-6343 Rotkreuz<br />
                     <br />
                     Switzerland
+                    <br /><br /><a href="https://realigned.ch" rel="noopener">https://realigned.ch</a>
                     </div>
                 </main>
 
